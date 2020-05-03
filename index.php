3a6e53ae5ca4f3586d0e113f54cbd954c17623ef
Jimmy262 <42589476+Jimmy262@users.noreply.github.com>
2020-05-03 11:18:39 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,431 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>EREII 2020</title>
+<!-- 
+
+
+
+-->
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+     <link rel="stylesheet" href="css/animate.css">
+     <link rel="stylesheet" href="css/owl.carousel.css">
+     <link rel="stylesheet" href="css/owl.theme.default.min.css">
+     <link rel="stylesheet" href="css/magnific-popup.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/templatemo-style.css">
+     <link rel="icon" href="images/e.png" type="image/png">
+
+
+</head>
+<body>
+
+     <!-- PRE LOADER -->
+     <section class="preloader">
+          <div class="spinner">
+
+               <span class="spinner-rotate"></span>
+               
+          </div>
+     </section>
+
+
+     <!-- MENU -->
+     <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+ 
+                    <!-- lOGO TEXT HERE -->
+                    <a href="index.html" class="navbar-brand">EREII <span>  </span> 2020</a>
+               </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-nav-first">
+                         <li><a href="#home" class="smoothScroll">INICIO</a></li>
+                         <li><a href="#about" class="smoothScroll">NOSOTROS</a></li>
+                         <li><a href="#team" class="smoothScroll">EJES TEMATICOS</a></li>
+                         <li><a href="#menu" class="smoothScroll">INSCRIPCION</a></li>
+                         <li><a href="#menu2" class="smoothScroll">CRONOGRAMA</a></li>
+                         
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                         
+                         <a href="https://www.facebook.com/Congresoereii" class="section-btn" attr="facebook icon">Visitanos en facebook</a>
+                         
+                    </ul>
+               </div>
+
+          </div>
+     </section>
+
+
+     <!-- HOME -->
+     <section id="home" class="slider" data-stellar-background-ratio="0.5">
+          <div class="row">
+
+                    <div class="owl-carousel owl-theme">
+                         <div class="item item-first">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-8 col-sm-12">
+                                             <img class="logo" src="images/ereii.png">
+                                             <h1> 2020</h1>
+                                             <h3>Encuentro Regional de Estudiantes de Ingeniería Industrial</h2>
+                                             <h3>Del 22 al 24 de Abril ¡¡No te lo pierdas!!</h3>
+                                             
+
+
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                        
+
+          </div>
+     </section>
+
+
+     <!-- ABOUT -->
+     <section id="about" data-stellar-background-ratio="0.5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-6 col-sm-12">
+                         <div class="about-info">
+                              <div class="section-title wow fadeInUp" data-wow-delay="0.2s">
+                                   <h4>Nosotros</h4>
+                                   <h2>¿Quienes somos?</h2>
+                              </div>
+
+                              <div class="wow fadeInUp" data-wow-delay="0.4s">
+                                   
+                                   <p class="parrafo">El Encuentro regional de Estudiantes de Ingeniería Industrial, en su tercera edición, es un evento académico que busca propiciar la integración de los estudiantes a través del intercambio permanente de información, descubriendo, despertando y potenciando en los participantes capacidades e incumbencias, desconocidas o desvalorizadas, que contribuyan a solucionar problemas concretos para el beneficio colectivo de toda una región y que además sean útiles para su futuro profesional.</p>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-6 col-sm-12">
+                         <div class="wow fadeInUp about-image" data-wow-delay="0.6s">
+                              <img src="images/about-image.jpg" class="img-responsive" alt="">
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </section>
+
+
+     <!-- TEAM -->
+     <section id="team" data-stellar-background-ratio="0.5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-12 col-sm-12">
+                         <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
+                              <h2>Nuestros ejes tematicos</h2>
+                              
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4">
+                         <div class="team-thumb wow fadeInUp" data-wow-delay="0.2s">
+                              <img src="images/team-image1.jpg" class="img-responsive" alt="">
+                                   <div class="team-hover">
+                                        <div class="team-item">
+                                             <h5 >"El objetivo del Supply Chain Management es maximizar el valor total generado y reducir los costos de toda la cadena. De esta manera, el superávit de la empresa estará en positivo y en crecimiento." </h5> 
+                                             <ul class="social-icon">
+                                                  
+                                             </ul>
+                                        </div>
+                                   </div>
+                         </div>
+                         <div class="team-info">
+                              <h3>Supply Chain</h3>
+                              
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4">
+                         <div class="team-thumb wow fadeInUp" data-wow-delay="0.4s">
+                              <img src="images/team-image2.jpg" class="img-responsive" alt="">
+                                   <div class="team-hover">
+                                        <div class="team-item">
+                                             <h4>“La calidad es ante todo prestar un servicio y satisfacer al cliente. Ocupémonos más del cliente y la calidad se ocupará de sí misma”</h4>
+                                             
+                                        </div>
+                                   </div>
+                         </div>
+                         <div class="team-info">
+                              <h3>Calidad</h3>
+                              
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4">
+                         <div class="team-thumb wow fadeInUp" data-wow-delay="0.6s">
+                              <img src="images/team-image3.jpg" class="img-responsive" alt="">
+                                   <div class="team-hover">
+                                        <div class="team-item">
+                                             <h4>“Los datos son la nueva ciencia. El Big Data son las respuestas”</h4>
+                                             
+                                        </div>
+                                   </div>
+                         </div>
+                         <div class="team-info">
+                              <h3>Big Data</h3>
+                              
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </section>
+
+
+     <!-- MENU-->
+     <section id="menu" data-stellar-background-ratio="0.5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-12 col-sm-12">
+                         <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
+                         <h2>¿Como inscribirse?</h2>
+                         <h3>Muy pronto</h3>
+                              
+                         </div>
+                    </div>
+
+                   
+
+
+               </div>
+          </div>
+     </section>
+
+     <section id="menu2" data-stellar-background-ratio="0.5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-12 col-sm-12"> 
+                         <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
+                              <h2>Nuestro cronograma</h2>
+                              
+                         </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                         <!-- MENU THUMB -->
+                         <div class="menu-thumb">
+                              <a href="images/cronograma.jpg" class="image-popup" title="Te esperamos es 22-23-24 de Abril">
+                                   <img src="images/cronograma.jpg" class="img-responsive" alt="">
+
+                                   <div class="menu-info">
+                                        <div class="menu-item">
+                                             <h3>¡¡Son 3 dias donde disfrutaras de muchas actividades!!</h3>
+                                             
+                                        </div>
+                                   </div>
+                              </a>
+                         </div>
+                    </div>
+
+
+               </div>
+          </div>
+     </section>
+
+
+
+     <!-- TESTIMONIAL -->
+     <section id="testimonial" data-stellar-background-ratio="0.5">
+          <div class="overlay"></div>
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-12 col-sm-12">
+                         <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
+                              <h2>Noticias</h2>
+                         </div>
+                    </div>  
+
+                    <div class="col-md-offset-2 col-md-8 col-sm-12">
+                         <div class="owl-carousel owl-theme">
+                              <div class="item">
+                                   <p>Felicitaciones Familia Industrial ¡¡¡ 
+                                        Nuestra fan page ha logrado los 3000 likes 
+                                        Sigamos creciendo y muchísimas gracias por su confianza  nuestro equipo organizador está cada vez más motivado a cumplir sus expectativas y a darles una experiencia inolvidable para este EREII 2020. 
+                                         </p>
+                                        <div class="tst-author">
+                                             <span>"La mejor forma de predecir el futuro es crearlo"</span>
+                                        </div>
+                              </div>
+
+                              <div class="item">
+                                   <p>¡Volvemos con fuerza! 
+                                        Les presentamos nuestros 3 ejes:
+                                         Supply Chain Management
+                                         Big Data
+                                         Calidad </p>
+                                        <div class="tst-author">
+                                             
+                                             <span>"Has lo que temes y venceras el miedo"</span>
+                                        </div>
+                              </div>
+
+                              
+
+                         </div>
+                    </div>
+
+               </div>
+          </div>
+     </section>  
+
+
+     <!-- CONTACT -->
+     <section id="contact" data-stellar-background-ratio="0.5">
+          <div class="container">
+               <div class="row">
+	<!-- How to change your own map point
+            1. Go to Google Maps
+            2. Click on your location point
+            3. Click "Share" and choose "Embed map" tab
+            4. Copy only URL and paste it within the src="" field below
+	-->
+                    <div class="wow fadeInUp col-md-6 col-sm-12" data-wow-delay="0.4s">
+                         <div id="google-map">
+                              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d975.6010533785184!2d-77.05062237705008!3d-12.015669120445102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105cf1933ebfa2d%3A0xa37f8073af2cc66b!2sFIIS%20-%20UNI!5e0!3m2!1sen!2spe!4v1583166482811!5m2!1sen!2spe" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                         </div>
+                    </div>    
+
+                    <!--
+                         <div class="col-md-6 col-sm-12">
+
+                         <div class="col-md-12 col-sm-12">
+                              <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
+                                   <h2>Contactanos</h2>
+                              </div>
+                         </div>
+                    -->
+                         <!-- CONTACT FORM -->
+                       <!--  <form action="#" method="post" class="wow fadeInUp" id="contact-form" role="form" data-wow-delay="0.8s">
+
+                              if email is succesful
+                              <h6 class="text-success">Your message has been sent successfully.</h6>
+                              
+                              IF MAIL NOT SENT 
+                              <h6 class="text-danger">E-mail must be valid and message must be longer than 1 character.</h6>
+
+                              <div class="col-md-6 col-sm-6">
+                                   <input type="text" class="form-control" id="cf-name" name="name" placeholder="Nombre completo">
+                              </div>
+
+                              <div class="col-md-6 col-sm-6">
+                                   <input type="email" class="form-control" id="cf-email" name="email" placeholder="Correo">
+                              </div>
+
+                              <div class="col-md-12 col-sm-12">
+                                   <input type="text" class="form-control" id="cf-subject" name="subject" placeholder="Asunto">
+
+                                   <textarea class="form-control" rows="6" id="cf-message" name="message" placeholder="Mensaje"></textarea>
+
+                                   <button type="submit" class="form-control" id="cf-submit" name="submit">Enviar Mensaje</button>
+                              </div>
+                         </form> -->
+                    </div>
+
+               </div>
+          </div>
+     </section>          
+
+
+     <!-- FOOTER -->
+    <!-- <footer id="footer" data-stellar-background-ratio="0.5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-3 col-sm-8">
+                         <div class="footer-info">
+                              <div class="section-title">
+                                   <h2 class="wow fadeInUp" data-wow-delay="0.2s">Find us</h2>
+                              </div>
+                              <address class="wow fadeInUp" data-wow-delay="0.4s">
+                                   <p>---</p>
+                              </address>
+                         </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-8">
+                         <div class="footer-info">
+                              <div class="section-title">
+                                   <h2 class="wow fadeInUp" data-wow-delay="0.2s">---</h2>
+                              </div>
+                              <address class="wow fadeInUp" data-wow-delay="0.4s">
+                                   <p>----</p>
+                                  
+                              </address>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-8">
+                         <div class="footer-info footer-open-hour">
+                              <div class="section-title">
+                                   <h2 class="wow fadeInUp" data-wow-delay="0.2s">Open Hours</h2>
+                              </div>
+                              <div class="wow fadeInUp" data-wow-delay="0.4s">
+                                   <p>----</p>
+                                   
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-4">
+                         <ul class="wow fadeInUp social-icon" data-wow-delay="0.4s">
+                              
+                              <li><a href="#" class="fa fa-twitter"></a></li>
+                              <li><a href="#" class="fa fa-instagram"></a></li>
+                              <li><a href="#" class="fa fa-google"></a></li>
+                         </ul>
+
+                         <div class="wow fadeInUp copyright-text" data-wow-delay="0.8s"> 
+                              <p>---</p>
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </footer>
+-->
+
+     <!-- SCRIPTS -->
+     <script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/jquery.stellar.min.js"></script>
+     <script src="js/wow.min.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+     <script src="js/jquery.magnific-popup.min.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+
+</body>
+</html>
